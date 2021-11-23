45e7615875c5b247093aa9875e6e0438b9b27dc1
Abdur Rashid <abdur363@gmail.com>
2021-11-23 22:40:04 +0600

    Upadating our app with a shop page, collection-preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import HomePage from './pages/homepage/HomePage.component';
-
+import {Route, Routes } from 'react-router-dom'
+import ShopPage
+ from './pages/shop/shop.component';
+const HatsPage = ()=>{
+  return(
+    <div>
+      This is Hats page
+    </div>
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div >
+      <Routes>
+        <Route exact path='/' element={<HomePage />}/>
+        <Route path='/hats' element={<HatsPage />}/>
+        <Route path='/shop' element={<ShopPage />}/>
+      </Routes>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import { useNavigate } from "react-router-dom"
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+
+const MenuItem = ({ title, imageUrl, size }) => {
+  const navigate=useNavigate();
+  function handleClick(){
+    navigate('/hats')
+  }
+  return(
+  <div className={`${size} menu-item`} onClick={handleClick}>
     <div
       className='background-image'
       style={{
@@ -15,6 +21,6 @@ const MenuItem = ({ title, imageUrl, size }) => (
       <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>
-);
+)}
 
 export default MenuItem;
